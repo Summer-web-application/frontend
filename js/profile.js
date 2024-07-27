0912096422aa056d0e99ad8a40e6dbe62cd47fdb
jasperkari <79049353+jasperkari@users.noreply.github.com>
2024-07-27 11:39:00 +0000

    Revert "edit to post without backend"

--- js/profile.js
+++ js/profile.js
@@ -1,7 +1,14 @@
 fetch('http://localhost:3000/api/user/1')
                 .then(response => response.json())
                 .then(data => {
+                    // TEMPORARY DATA NEED TO SWITCH TO USING THE DATABASE LATER
+                    data.bio = "temp"
+                    data.profilePicture = "https://divedigital.id/wp-content/uploads/2022/07/2-Blank-PFP-Icon-Instagram.jpg"
+
+
+                    document.getElementById('profile-picture').src = data.profilePicture;
                     document.getElementById('user-name').innerText = data[0].first_name + " " + data[0].last_name;
+                    document.getElementById('user-bio').innerText = data.bio;
                 })
                 .catch(error => console.error('Error cant get user profile:', error));
 
@@ -12,10 +19,12 @@ fetch('http://localhost:3000/api/user/posts/1')
         const postTemplate = postsContainer.querySelector('.post');
 
         posts.forEach(post => {
-            const postElement = postTemplate.cloneNode(true);
-            postElement.classList.remove('template');
-            postElement.querySelector('.post-text').innerText = post.text;
+            // TEMPORARY DATA NEED TO SWITCH TO USING THE DATABASE LATER
+            post.time = "May 1, 2024";
 
+            const postElement = postTemplate.cloneNode(true);
+            postElement.querySelector('.post-text').innerText = post.text;
+            postElement.querySelector('.post-timestamp').innerText = post.time;
             postElement.addEventListener('click', function () {
                 window.location.href = `post.html?postId=${post.id}`;
             });
